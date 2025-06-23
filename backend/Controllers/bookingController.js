ca3288ab7ac5cf84df8bd56949d5f8752767db49
Ayush Patel <iamayushpatelap@gmail.com>
2025-06-23 11:00:19 +0530

    Done

--- backend/Controllers/bookingController.js
+++ backend/Controllers/bookingController.js
@@ -1,6 +1,6 @@
 import User from "../models/UserSchema.js";
 import Doctor from "../models/DoctorSchema.js";
-import Booking from "../models/bookingSchema.js";
+import Booking from "../models/BookingSchema.js";
 import Stripe from "stripe";
 
 export const getCheckoutSession = async (req, res) => {
